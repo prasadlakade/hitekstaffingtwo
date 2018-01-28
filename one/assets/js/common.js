1f02e10822fb8565d9737f76f7c50dd54a4ba294
prasad.l <prasad.l@media.net>
2018-01-28 22:26:50 +0530

    versions added

---
+++ one/assets/js/common.js
@@ -0,0 +1,13 @@
+$('.bannerslider').owlCarousel({
+    loop:true,
+    //autoplayTimeout:5000,
+    //autoplay:true,
+    smartSpeed:900,
+    items:1
+})
+
+$('.clientslider').owlCarousel({
+    loop:true,
+    autoplay:true,
+    items:4
+})
